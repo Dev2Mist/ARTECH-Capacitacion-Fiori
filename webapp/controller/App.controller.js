bf972a1fd190b90785b310d2b9ce0673fc46974b
Dev2Mist <benjaminborges.dev@gmail.com>
2025-12-08 19:38:50 -0300

    :sparkles:Ejercicio finalizado, se puede ingresar un input y se muestra por pantalla con MessageToast

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,21 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+],
+  
+  (Controller, MessageToast) => {
   "use strict";
 
   return Controller.extend("artech.capacitacion.fiori.controller.App", {
-    onDisplayHelloWorld() {
-      alert("Hola Mundo");
-    },
+    onMostrarSaludo() {
+
+      const input = this.getView().byId("inputUsuario");
+
+      if (input.getValue().length === 0) MessageToast.show("¡Has ingresado un input vacío!");
+      else {
+        MessageToast.show(`Hola ${input.getValue()}`);
+      }
+
+    }
   });
 });
