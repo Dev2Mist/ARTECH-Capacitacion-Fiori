e5159290ff693f64aa16be5dbe6f5a73e935cefe
Dev2Mist <benjaminborges.dev@gmail.com>
2025-12-08 22:03:25 -0300

    Subo el proyecto ya configurado con el wizard

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["artech/capacitacion/fiori/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"artech/capacitacion/fiori/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
